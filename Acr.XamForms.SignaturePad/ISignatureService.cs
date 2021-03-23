2820eb170935adcc179cfa947f13e12903f37ccb
Ndumiso_suspect <73614263+Bhekinkosi12@users.noreply.github.com>
2021-03-23 14:19:44 -0700

    Update ISignatureService.cs

--- Acr.XamForms.SignaturePad/ISignatureService.cs
+++ Acr.XamForms.SignaturePad/ISignatureService.cs
@@ -7,6 +7,8 @@ namespace Acr.XamForms.SignaturePad {
 
     public interface ISignatureService {
 
+        
         Task<SignatureResult> Request(SignaturePadConfiguration config = null, CancellationToken cancelToken = default(CancellationToken));
+    
     }
 }
